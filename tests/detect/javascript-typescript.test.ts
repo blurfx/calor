9e0ad3151d97c3d68866b5be994fe67ede48c623
blurfx <iam@xo.dev>
2023-06-15 00:04:15 +0900

    refactor: separate language detection tests by language

---
+++ tests/detect/javascript-typescript.test.ts
@@ -0,0 +1,61 @@
+import { describe, it, assert } from 'vitest';
+import { detectLanguage } from '../../src';
+
+describe('javascript/typescript language detection', () => {
+  it('case 1', () => {
+    const code = 'console.log("hello world")';
+    const lang = detectLanguage(code);
+    assert.include(['javascript', 'typescript'], lang);
+  });
+
+  it('case 2', () => {
+    const code = 'const a = 1';
+    const lang = detectLanguage(code);
+    assert.include(['javascript', 'typescript'], lang);
+  });
+
+  it('case 3', () => {
+    const code = `
+      function foo() {
+        const text = 'hello world';
+        console.log(text);
+      }
+      `;
+    const lang = detectLanguage(code);
+    assert.include(['javascript', 'typescript'], lang);
+  });
+
+  it('case 4', () => {
+    const code = `
+      function foo() {
+        const text: string = 'hello world';
+        console.log(text);
+      }
+      `;
+    const lang = detectLanguage(code);
+    assert.equal('typescript', lang);
+  });
+
+  it('case 4', () => {
+    const code = `
+      type str = string;
+      function foo() {
+        const text: str = 'hello world';
+        console.log(text);
+      }
+      `;
+    const lang = detectLanguage(code);
+    assert.equal('typescript', lang);
+  });
+
+  it('case 5', () => {
+    const code = `
+      import * as fs from 'fs';
+
+      const text = fs.readFileSync('foo.txt', 'utf8');
+      console.log(text);
+      `;
+    const lang = detectLanguage(code);
+    assert.include(['typescript', 'javascript'], lang);
+  });
+});
