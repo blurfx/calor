8bad548d6c7c7140fe79252879c0c2f4ddd834e7
changhui lee <iam@xo.dev>
2023-08-07 12:51:33 +0900

    feat: export ParseRule type

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,4 +2,4 @@ export * from './detect';
 export * from './tokenizer';
 export * from './constants';
 export * from './rules';
-export type { SegmentKind, Token } from './types';
+export type { SegmentKind, Token, ParseRule } from './types';
