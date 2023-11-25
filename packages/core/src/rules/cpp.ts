036156bcb74b88e75fdf396d0fcf149a5b5becaf
blurfx <iam@xo.dev>
2023-11-26 04:39:33 +0900

    fix: c/c++ directive and preprocessor tokenize

--- packages/core/src/rules/cpp.ts
+++ packages/core/src/rules/cpp.ts
@@ -7,6 +7,15 @@ const cppRules: ParseRule[] = [
   },
   {
     kind: 'keyword',
+    pattern: /(#\s*include|ifdef|ifndef|elidef|elifndef)(\s*)(<.*>|".*")/g,
+    matchHints: ['keyword', 'plain', 'string'],
+  },
+  {
+    kind: 'keyword',
+    pattern: /(#\s*(?:define|if|elif|else|endif|import|error|line))\b/g,
+  },
+  {
+    kind: 'keyword',
     pattern:
       /\b(auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while|bool|true|false|catch|class|const_cast|delete|dynamic_cast|explicit|export|friend|inline|mutable|namespace|new|operator|private|protected|public|reinterpret_cast|static_cast|template|this|throw|try|typeid|typename|using|virtual|nullptr|null|string|and|and_eq|bitand|bitor|compl|not|not_eq|or|or_eq|xor|xor_eq)\b/g,
   },
